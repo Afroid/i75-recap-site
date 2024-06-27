1d2c799dc37676704cec1be99c9514c09a8e872c
The John <jsqueet08@gmail.com>
2024-06-27 16:19:21 -0400

    Pushing CNAME, updated content in index, updated app test, changed app with Pioli and updated the link, added Pioli asset.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import Pioli from './Pioli.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Pioli} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {/* Edit <code>src/App.tsx</code> and save to reload. */}
+          Coming soon...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://fantasy.espn.com/football/league?leagueId=849836"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I75 League
         </a>
       </header>
     </div>
