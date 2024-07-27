1716b2c1d69e3fe6d1a51f5ae5c13b510c832eca
The John <jsqueet08@gmail.com>
2024-07-26 23:41:28 -0400

    Trying out an initial navbar with styling-component. Will probably remove styling-component and convert to TSX once I decide on final navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import Pioli from './Pioli.png';
+// import Pioli from './Pioli.png';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={Pioli} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.tsx</code> and save to reload. */}
-          Coming soon...
-        </p>
-        <a
-          className="App-link"
-          href="https://fantasy.espn.com/football/league?leagueId=849836"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I75 League
-        </a>
-      </header>
+      <Navbar/>
     </div>
   );
 }
