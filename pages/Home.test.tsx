0e86943175304b162ad78208e3692b869c9248fd
The John <jsqueet08@gmail.com>
2025-05-17 19:00:28 -0400

    Renamed nine files because I added a config for next so that way only specific extensions are treated as pages.

--- pages/Home.test.tsx
+++ pages/Home.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index';
+import Home from '@/pages/index.page';
 import { TestIds } from '@/lib/testIds';
 
 describe('Home page', () => {
